70b4180d11c812f25c1468ddb607020527d4ee27
Reyan Shariff <reyan.shariff@duke.edu>
2024-07-07 21:23:21 -0400

    implemented screens and screen navigation

---
+++ interval-training/screens/HomeScreen.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Spacer = ({ width }) => <View style={{ width }} />;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    space: { //Creates space between the buttons
+      width: 20, // or whatever size you need
+      height: 20,
+    },
+    input: {
+      marginTop: 20,
+    },
+  });
+
+export default function HomeScreen({navigation}) 
+{
+
+    return (<View style={styles.container}>
+        <Text>Interval Trainer</Text>
+        <StatusBar style="auto" />
+        <View style={styles.input}>
+          <Button title="Test Your Knowledge" onPress={() => navigation.navigate('Test')} />
+          <View style={styles.space} />
+          <Button title="Review" onPress={() => navigation.navigate('Review')} />
+        </View>
+      </View>);
+}
---
+++ interval-training/screens/ReviewMode.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+
+export default function ReviewMode({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Review Mode</Text>
+    </View>
+  );
+}
+
+// ... other code from the previous section
