60ac992c4c5e8885249c719268ff0e598ed4585f
Reyan Shariff <reyan.shariff@duke.edu>
2024-08-04 12:53:49 -0400

    Randomized starting note

--- interval-training/screens/TestMode.jsx
+++ interval-training/screens/TestMode.jsx
@@ -18,10 +18,33 @@ const TestMode = ({ navigation }) => {
     // Add navigation actions here if needed
   };
 
-  async function playSound() {
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  const sounds = {
+    'A3': require('../assets/piano-mp3/A3.mp3'),
+    'Ab3': require('../assets/piano-mp3/Ab3.mp3'),
+    'B3': require('../assets/piano-mp3/B3.mp3'),
+    'Bb3': require('../assets/piano-mp3/Bb3.mp3'),
+    'C3': require('../assets/piano-mp3/C3.mp3'),
+    'D3': require('../assets/piano-mp3/D3.mp3'),
+    'Db3': require('../assets/piano-mp3/Db3.mp3'),
+    'E3': require('../assets/piano-mp3/E3.mp3'),
+    'Eb3': require('../assets/piano-mp3/Eb3.mp3'),
+    'F3': require('../assets/piano-mp3/F3.mp3'),
+    'G3':require('../assets/piano-mp3/G3.mp3'),
+    'Gb3': require('../assets/piano-mp3/Gb3.mp3')
+  };
+  
+
+  async function playSound() { //Gives you the starting note using the expo audio api
     console.log('Loading Sound');
-    const { sound } = await Audio.Sound.createAsync( require('../assets/piano-mp3/A3.mp3')
-    );
+    const keys = Object.keys(sounds);
+    const randomIndex = Math.floor(Math.random() * keys.length);
+    const randomKey = keys[randomIndex];
+    const randomPath = sounds[randomKey];
+   const { sound } = await Audio.Sound.createAsync(randomPath);
     setSound(sound);
 
     console.log('Playing Sound');
